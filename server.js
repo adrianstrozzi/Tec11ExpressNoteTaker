6cc0ae8e4d1011aef265ce7828656c06ac5c41ee
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-05-28 16:11:07 -0500

    Fixed paths

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001
 
-const mainPath = path.join(__dirname, "/public")
-
 const notes = require('./db/db.json');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(mainPath, 'index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(mainPath, 'notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
