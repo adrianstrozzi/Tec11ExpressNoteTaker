438579ba1a9d9a6cd620b8c6c363265494f90581
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-05-28 16:06:38 -0500

    Created path variable and fixed notes.html <a>

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const mainPath = path.join(__dirname, "/public")
+
 const notes = require('./db/db.json');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(mainPath, 'index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(mainPath, 'notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
